028b9c71de528d10785d7bdb5466d570f681ae47
Adrienne Galloway <adriennegalloway@hotmail.com>
2022-01-28 17:18:42 +0000

    adding a description for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:image, :image_cache)
+    params.require(:post).permit(:description, :image, :image_cache)
   end 
 
 end 
