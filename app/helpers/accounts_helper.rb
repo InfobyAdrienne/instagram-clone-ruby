04d187aa066f4d180624e7a07832fb393e3d3741
Adrienne Galloway <aasgalloway@gmail.com>
2022-02-07 18:02:15 +0000

    creating a follower suggestion box on the dashboard containing all created users

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,8 @@
 module AccountsHelper
+
+  def profile_picture account, width = 40
+    image_path = account.image.present? ? account.image.thumb.url : "default.png"
+    image_tag(image_path, width: width, class: "img-circle")
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.create([
+  { username: "johndoe", first_name: "John", last_name: "Doe", email: "john@test.com", password: "passowrd" }, 
+  { username: "janesmith", first_name: "Jane", last_name: "Smith", email: "jane@test.com", password: "passowrd" }, 
+  { username: "suebaker", first_name: "Sue", last_name: "Baker", email: "sue@test.com", password: "passowrd" }, 
+  ])
+
