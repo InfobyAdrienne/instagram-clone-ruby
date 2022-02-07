76f6bcfa3c960c5c019dd08d9ad297b47afdc21d
Adrienne Galloway <aasgalloway@gmail.com>
2022-02-07 17:10:13 +0000

    the likes counter goes up and down as it is clicked

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -19,6 +19,8 @@ class LikesController < ApplicationController
       else 
         @success = false
       end 
+
+       @post_likes = Post.find(@post_id).total_likes_count
        render 'posts/like' 
       }
     end
