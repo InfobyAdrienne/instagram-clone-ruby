874d77a5dd66712d97cb7bc2013ea079863e712c
Adrienne Galloway <aasgalloway@gmail.com>
2022-02-15 16:40:28 +0000

    fixing the blurry profile photo

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,7 +1,8 @@
 module AccountsHelper
 
   def profile_picture account, width = 40
-    image_path = account.image.present? ? account.image.thumb.url : "default.png"
+    image_path = account.image.present? ? account.image.url : "default.png"
+    # this was previously account.image.thumb.url but it was blurry
     image_tag(image_path, width: width, class: "img-circle")
   end
 
